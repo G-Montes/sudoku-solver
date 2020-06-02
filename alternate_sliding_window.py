9db114cae21f6d5e65b97c308ab0a28de48df9e2
GenardM <genardmontes@gmail.com>
2020-06-02 16:17:26 -0500

    Right now, the current implementation of sliding window is convoluted and not scalable. This implementation will work to allow different types of sudoku images to be added.

--- alternate_sliding_window.py
+++ alternate_sliding_window.py
@@ -1,13 +1,9 @@
+import sys
 import cv2 as cv
 import numpy
 
 sudoku_img_path = r"C:\\Users/genar/Documents/431.jpg"
-
 THRESHOLD = 75
-window_name = 'Edge Map'
-ratio = 3
-kernel_size = 3 
-matrix = (3, 3)
 
 def load_img(image_path):
     image = cv.imread(sudoku_img_path)
@@ -17,15 +13,21 @@ def load_img(image_path):
         return image
 
 def get_edges(thresh):
+    ratio = 3
+    kernel_size = 3 
+    matrix = (3, 3)
     img_blur = cv.blur(sudoku_img_gray, matrix)
     detected_edges = cv.Canny(img_blur, thresh, thresh * ratio, kernel_size)
     mask = detected_edges != 0
     edges = sudoku_img * (mask[:,:, None].astype(sudoku_img.dtype))
-    cv.imshow(window_name, edges)
     return edges
 
 sudoku_img = load_img(sudoku_img_path)
 sudoku_img_gray = cv.cvtColor(sudoku_img, cv.COLOR_BGR2GRAY)
-cv. namedWindow(window_name)
 sudoku_img_edges = get_edges(THRESHOLD)
+altered = sudoku_img_edges[:,:,0]
+print(altered.shape)
+cv.imshow("image", altered)
 cv.waitKey(0)
+
+            
