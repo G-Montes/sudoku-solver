af52696d2319d69ab16eb01ae6358a3e11d9743e
GenardM <genardmontes@gmail.com>
2020-06-03 10:54:18 -0500

    Added function that crops to the sudoku board, which should make it easier to implement a sliding window.

--- alternate_sliding_window.py
+++ alternate_sliding_window.py
@@ -5,6 +5,10 @@ import numpy
 sudoku_img_path = r"C:\\Users/genar/Documents/431.jpg"
 THRESHOLD = 75
 
+def show_image(image):
+    cv.imshow("", image)
+    cv.waitKey(0)
+
 def load_img(image_path):
     image = cv.imread(sudoku_img_path)
     if image is None:
@@ -22,12 +26,26 @@ def get_edges(thresh):
     edges = sudoku_img * (mask[:,:, None].astype(sudoku_img.dtype))
     return edges
 
+def find_sudoku_board_corner(image):
+    for row_index, row in enumerate(image):
+        if numpy.any(row):
+            for col_index, element in enumerate(row):
+                if element != 0:
+                    print(row_index, col_index)
+                    return (row_index, col_index)
+
+def crop_to_sudoku_border(image):
+    num_rows, num_cols = image.shape
+    top_left_corner =  find_sudoku_board_corner(image)
+    reversed_img = image[::-1,::-1]
+    bottom_right_corner = find_sudoku_board_corner(reversed_img)
+    
+    return image[top_left_corner[0]:num_rows - bottom_right_corner[0], top_left_corner[1]: num_cols - bottom_right_corner[1]]
+
 sudoku_img = load_img(sudoku_img_path)
 sudoku_img_gray = cv.cvtColor(sudoku_img, cv.COLOR_BGR2GRAY)
 sudoku_img_edges = get_edges(THRESHOLD)
 altered = sudoku_img_edges[:,:,0]
-print(altered.shape)
-cv.imshow("image", altered)
-cv.waitKey(0)
-
-            
+cropped_image = crop_to_sudoku_border(altered)
+print(cropped_image.shape)
+show_image(cropped_image)
