d42876db2f960c045a0e5b8526b5ddcddd82c6d2
GenardM <genardmontes@gmail.com>
2021-01-26 10:57:28 -0600

    Streamlined load_img function to have grayscaling turned on by default. Simplified image loading.

--- alternate_sliding_window.py
+++ alternate_sliding_window.py
@@ -9,21 +9,22 @@ def show_image(image):
     cv.imshow("", image)
     cv.waitKey(0)
 
-def load_img(image_path):
-    image = cv.imread(sudoku_img_path)
+def load_img(image_path,use_grayscale = True):
+    image = cv.imread(sudoku_img_path, 0 if use_grayscale else 1)
+    
     if image is None:
         raise Exception("Something went wrong with the image loading.")
     else:
         return image
 
-def get_edges(thresh):
+def get_edges(thresh, image):
     ratio = 3
     kernel_size = 3 
     matrix = (3, 3)
-    img_blur = cv.blur(sudoku_img_gray, matrix)
+    img_blur = cv.blur(image, matrix)
     detected_edges = cv.Canny(img_blur, thresh, thresh * ratio, kernel_size)
     mask = detected_edges != 0
-    edges = sudoku_img * (mask[:,:, None].astype(sudoku_img.dtype))
+    edges = sudoku_img * (mask[:,:, None].astype(image.dtype))
     return edges
 
 def find_sudoku_board_corner(image):
@@ -31,7 +32,6 @@ def find_sudoku_board_corner(image):
         if numpy.any(row):
             for col_index, element in enumerate(row):
                 if element != 0:
-                    print(row_index, col_index)
                     return (row_index, col_index)
 
 def crop_to_sudoku_border(image):
@@ -43,9 +43,11 @@ def crop_to_sudoku_border(image):
     return image[top_left_corner[0]:num_rows - bottom_right_corner[0], top_left_corner[1]: num_cols - bottom_right_corner[1]]
 
 sudoku_img = load_img(sudoku_img_path)
-sudoku_img_gray = cv.cvtColor(sudoku_img, cv.COLOR_BGR2GRAY)
-sudoku_img_edges = get_edges(THRESHOLD)
-altered = sudoku_img_edges[:,:,0]
-cropped_image = crop_to_sudoku_border(altered)
-print(cropped_image.shape)
-show_image(cropped_image)
+# sudoku_img_edges = get_edges(THRESHOLD, sudoku_img)
+
+# contours, hierarchy = cv.findContours(sudoku_img, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+
+# cv.drawContours(sudoku_img, contours, -1, (0, 255, 0), 3)
+
+# show_image(sudoku_img)
+
