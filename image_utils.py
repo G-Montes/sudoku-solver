4bf37f22a27398f883f1988ef1218295aa2759e3
G-Montes <genardmontes@gmail.com>
2021-12-21 16:00:48 -0600

    Moved functions from edge_finder that served a utility purpose.

--- image_utils.py
+++ image_utils.py
@@ -1,5 +1,19 @@
 import os
 import json
+import cv2 as cv
+import numpy.typing as npt
+
+
+def show_image(img: npt.NDArray, name: str = "") -> None:
+    cv.imshow(name, img)
+
+
+def load_img(image_path: str) -> npt.NDArray:
+    image = cv.imread(image_path)
+    if image is None:
+        raise Exception("Something went wrong with the image loading.")
+    else:
+        return image
 
 
 def get_image_path(image_name: str, use_solved_folder: bool) -> str:
