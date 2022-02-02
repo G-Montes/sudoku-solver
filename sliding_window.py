7d86a9a3a0e538c83ca0dfd2a6f5f2fcbefe0fde
G-Montes <genardmontes@gmail.com>
2022-02-02 15:38:49 -0600

    In progress changes. WFH

--- sliding_window.py
+++ sliding_window.py
@@ -1,12 +1,26 @@
-from corner_finder import find_harris_corners
-from edge_finder import get_image_edges
-from image_utils import get_image_path, show_image, load_img
-import cv2 as cv
+import cv2 as cv  # type: ignore
+import numpy
+import numpy.typing
 
-image_path = get_image_path("unsolved_1.png", use_solved_folder=False)
-image = load_img(image_path)
-edges = get_image_edges(image)
-corners = find_harris_corners(edges)
+import corner_finder
+import edge_finder
+import image_utils
 
-cv.imshow("corners", corners)
-cv.waitKey(0)
+
+def find_boxes(sudoku_img: numpy.typing.NDArray) -> tuple[int, int]:
+    # TODO: Add functionality to detect boundaries of a sudoku box
+    edges = edge_finder.get_image_edges(sudoku_img)
+    corners = corner_finder.find_harris_corners(edges)
+    edge = corner_finder.find_intersections(edges)
+
+    # image_utils.show_image(edges, "edges")
+    # image_utils.show_image(corners, "corners")
+    # image_utils.show_image(edge, "intersection")
+    cv.waitKey(0)
+
+    return (0, 0)
+
+
+image_path = image_utils.get_image_path("solved_1.png", use_solved_folder=True)
+sudoku_image = image_utils.load_img(image_path)
+find_boxes(sudoku_image)
